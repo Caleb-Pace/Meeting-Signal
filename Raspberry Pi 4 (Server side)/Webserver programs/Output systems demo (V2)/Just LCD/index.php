8dcf210a3e62a044e5b9e41761059a81ced86c98
C0D3Crafter <100537549+Caleb-Pace-School@users.noreply.github.com>
2022-08-18 12:29:01 +1200

    LCD fix

--- Raspberry Pi 4 (Server side)/Webserver programs/Output systems demo (V2)/Just LCD/index.php
+++ Raspberry Pi 4 (Server side)/Webserver programs/Output systems demo (V2)/Just LCD/index.php
@@ -3,6 +3,6 @@
 //=/ Parameter meanings
 // lcd_line_1: LCD character display line 1
 // lcd_line_2: LCD character display line 2
-shell_exec("sudo python output.py ".$_GET["lcd_line_1"]." ".$_GET["lcd_line_2"]);
+shell_exec("sudo -E python output.py ".$_GET["lcd_line_1"]." ".$_GET["lcd_line_2"]);
 
 ?>
