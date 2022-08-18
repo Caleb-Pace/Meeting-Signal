6e09dfbacce132118a73975afadad50b940fee51
C0D3Crafter <100537549+Caleb-Pace-School@users.noreply.github.com>
2022-08-18 13:32:45 +1200

    Debugging mode

--- Raspberry Pi 4 (Server side)/Webserver programs/Output systems demo (V2)/Just LCD/index.php
+++ Raspberry Pi 4 (Server side)/Webserver programs/Output systems demo (V2)/Just LCD/index.php
@@ -3,6 +3,10 @@
 //=/ Parameter meanings
 // lcd_line_1: LCD character display line 1
 // lcd_line_2: LCD character display line 2
-shell_exec("sudo -E python output.py ".$_GET["lcd_line_1"]." ".$_GET["lcd_line_2"]);
+echo "Line 1: \"".$_GET["lcd_line_1"]."\"<br/>";
+echo "Line 2: \"".$_GET["lcd_line_2"]."\"<br/>";
+echo "Command: \""."sudo -E python output.py ".$_GET["lcd_line_1"]." ".$_GET["lcd_line_2"]."\"<br/><br/>Output:<br/>";
+// shell_exec("sudo -E python output.py ".$_GET["lcd_line_1"]." ".$_GET["lcd_line_2"]);
+var_dump(shell_exec("sudo -E python output.py ".$_GET["lcd_line_1"]." ".$_GET["lcd_line_2"]." 2>&1"));
 
 ?>
--- Raspberry Pi 4 (Server side)/Webserver programs/Output systems demo (V2)/Just LCD/output.py
+++ Raspberry Pi 4 (Server side)/Webserver programs/Output systems demo (V2)/Just LCD/output.py
@@ -6,11 +6,14 @@ from rpi_lcd import LCD
 # Output
 def main():
     # LCD character display
+    print("Args: {}".format(len(sys.argv)))  # TEMP
     if len(sys.argv) > 1:
         lcd = LCD()
         lcd.text(sys.argv[1], 1)
+        print("L1: \"{}\"".format(sys.argv[1]))  # TEMP
     if len(sys.argv) > 2:
         lcd.text(sys.argv[2], 2)
+        print("L2: \"{}\"".format(sys.argv[2]))  # TEMP
 
 
 if __name__ == "__main__":
